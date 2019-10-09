07f88b15dae505eee90b5d11717799ae5d30ca02
Eduarda <meduardagalindo8@gmail.com>
2019-10-09 19:25:26 -0300

    calculus of the global K and initializing elementary force vector

--- Python_Codes/FEM.py
+++ Python_Codes/FEM.py
@@ -1,15 +1,42 @@
 import numpy as np
+#from scipy.integrate import quad
 
-class Kelem:
-    def Kel(n,A,E,he):
-        if n == 3:
-            k = 1/3*np.array([[7, -8, 1],[-8, 16, -8],[1, -8, 7])
-        if n == 2:
-            k = np.array([[1 -1],[-1, 1]])
-        Kel = A*E/he*k
+class Elem:
+    def hel(x1,x2,n_nos_el):
+        hel = (x2-x1)/n_nos_el
+        return hel
+
+    def Kel(n_nos_el,integralAdx,E,he):
+        if n_nos_el == 3:
+            k = 1./3*np.array([[7., -8., 1.],[-8., 16., -8.],[1., -8., 7.]])
+        if n_nos_el == 2:
+            k = np.array([[1., -1.],[-1., 1.]])
+        Kel = (E/he**2)*integralAdx*k    #quad(A,x1,x2)
         return Kel
-class FatorForma:
-    def N1(xe,xe1,xe2):return 1 - 3*((xe-xe1)/(xe2-xe1))**2 + 2*((xe-xe1)/(xe2-xe1))**3
-    def N2(xe,xe1,xe2):return -3*(xe-xe1)*(1-(xe-xe1)/(xe2-xe1))**2
-    def N3(xe,xe1,xe2):return 3*((xe-xe1)/(xe2-xe1))**2 - 2*((xe-xe1)/(xe2-xe1))**3
-    def N4(xe,xe1,xe2):return -(xe-xe1)*(((xe-xe1)/(xe2-xe1))**2 - (xe-xe1)/(xe2-xe1))
+
+    def Kg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec):
+        Kg = np.zeros([n_nos_tot,n_nos_tot])
+        ##conec = np.array([[1,2],[2,3]])
+        for el in range(0,n_el):
+            he = Elem.hel(x[el], x[el+1], n_nos_el[el]-1)
+            Kel = Elem.Kel(n_nos_el[el],integralAdx(he,el),E[el],he)
+            for i in range(0,n_nos_el[el]):
+                ig = conec[el,i] - 1
+                #if el>0 and i == 0: ig = ig
+                for j in range(0,n_nos_el[el]):
+                    jg = conec[el,j] - 1
+                    Kg[ig,jg] = Kg[ig,jg] + Kel[i,j]
+        return Kg
+
+    def Fel(n_nos_tot):
+        for i in range(n_nos_el):
+            if n_nos_el == 3: aa
+            if n_nos_el == 2:
+                Fel = f*he/2*[1, 1]
+
+
+#class FatorForma:
+#    def N1(xe,xe1,xe2):return 1 - 3*((xe-xe1)/(xe2-xe1))**2 + 2*((xe-xe1)/(xe2-xe1))**3
+#    def N2(xe,xe1,xe2):return -3*(xe-xe1)*(1-(xe-xe1)/(xe2-xe1))**2
+#    def N3(xe,xe1,xe2):return 3*((xe-xe1)/(xe2-xe1))**2 - 2*((xe-xe1)/(xe2-xe1))**3
+#    def N4(xe,xe1,xe2):return -(xe-xe1)*(((xe-xe1)/(xe2-xe1))**2 - (xe-xe1)/(xe2-xe1))
--- Python_Codes/Python_Tests/TesteFEM.py
+++ Python_Codes/Python_Tests/TesteFEM.py
@@ -1,19 +1,22 @@
 import numpy as np
-from ..FEM import Kelem
+from ..FEM import Elem
 import unittest
 
 class Test_FEM(unittest.TestCase):
-    def FEM_1GL(self): # 1grau de liberdade por nó - cálculo do deslocamento apenas
+    def testFEM_1GL(self): # 1grau de liberdade por nó - cálculo do deslocamento apenas
     # ENTRADA DE DADOS
-        n_el = 2; #numero de elementos da estrutura
-        n_nos_AB = 3; #número de nós em cada elemento
-        n_nos_BC = 2;
-        n_nos_tot = n_nos_AB*1 + n_nos_BC*1-(n_el-1); #número total de nós da estrutura
-        A = np.array([0.1, 0.1, 100])
-        E = np.array([200, 200, 200])
-        he = np.array([])
-        Ngl_no = 1;
+        n_el = 2; #numero de elementos da estrutura (AB e BC)
+        n_nos_el = np.array([3,2]); #número de nós por elemento
+        x = np.array([1., 3., 5.]); xA = x[0]; xB = x[1]; xC = x[2]
+        n_nos_tot = sum(n_nos_el) - (n_el-1); #número total de nós da estrutura
 
-    # INICIALIZAÇÃO:
-        for i in range(n_el):
-            Kel = Kelem.Kel(n_nos_el,A(i),E(i),he(i))
+        E = 2e7*np.ones(n_el);
+
+        def integralAdx(he,i):
+            if i==0: return 0.1*he
+            else: return 0.3
+
+        conec = np.array([[1,2,3], [3,4,0]])    ##barra AB tem 3nós e a BC tem 2. - caso tenha alguma outra coisa doida assim, preferi deixar logo pra o usuario definir
+    # INICIALIZAÇÃO
+        Kg = Elem.Kg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec)
+        print(Kg)
