41a2b7f1ffc9007b0d74fd2b9d874aa771239056
Eduarda <meduardagalindo8@gmail.com>
2019-10-11 15:33:03 -0300

    include the deslocation calculus of the shaft with a successfull result :)

--- Python_Codes/FEM.py
+++ Python_Codes/FEM.py
@@ -2,40 +2,74 @@ import numpy as np
 #from scipy.integrate import quad
 
 class Elem:
-    def hel(x1,x2,n_nos_el):
-        hel = (x2-x1)/n_nos_el
-        return hel
 
-    def Kel(n_nos_el,integralAdx,E,he):
+    def Kel_Fel(n_nos_el,integralAdx,E,he,f):
         if n_nos_el == 3:
             k = 1./3*np.array([[7., -8., 1.],[-8., 16., -8.],[1., -8., 7.]])
+            fel = np.array([1./6, 2./3, 1./6])
+
         if n_nos_el == 2:
             k = np.array([[1., -1.],[-1., 1.]])
-        Kel = (E/he**2)*integralAdx*k    #quad(A,x1,x2)
-        return Kel
+            fel = np.array([1./2, 1./2])
 
-    def Kg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec):
+        Kel = (E/he**2)*integralAdx*k    #quad(A,x1,x2)
+        Fel = f*he*fel
+
+        return Kel,Fel
+
+class Global:
+    def Kg_Fg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec,Fc,f):
         Kg = np.zeros([n_nos_tot,n_nos_tot])
+        Fg = np.zeros(n_nos_tot)
+
         ##conec = np.array([[1,2],[2,3]])
         for el in range(0,n_el):
-            he = Elem.hel(x[el], x[el+1], n_nos_el[el]-1)
-            Kel = Elem.Kel(n_nos_el[el],integralAdx(he,el),E[el],he)
+            he = abs(x[el] - x[el+1])
+            Kel, Fel = Elem.Kel_Fel(n_nos_el[el],integralAdx(he,el),E[el],he,f[el])
+
             for i in range(0,n_nos_el[el]):
                 ig = conec[el,i] - 1
+                Fg[ig] = Fg[ig] + Fel[i]
                 #if el>0 and i == 0: ig = ig
                 for j in range(0,n_nos_el[el]):
                     jg = conec[el,j] - 1
                     Kg[ig,jg] = Kg[ig,jg] + Kel[i,j]
-        return Kg
 
-    def Fel(n_nos_tot):
-        for i in range(n_nos_el):
-            if n_nos_el == 3: aa
-            if n_nos_el == 2:
-                Fel = f*he/2*[1, 1]
+        Fg = Fg + Fc
+        return Kg, Fg
 
 
-#class FatorForma:
+    def Kg_Fg_contorno(n_nos_tot,n_el,n_nos_el,x,integralAdx,E,conec,Fc,f,xCC,valor_CC):
+        CC = np.zeros(n_nos_tot)
+        Kg, Fg = Global.Kg_Fg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec,Fc,f)
+
+        for i in range(0,len(xCC)):
+            CC[xCC[i]] = 1
+            Fg[xCC[i]] = valor_CC[i]
+            Kg[xCC[i],xCC[i]] = 1
+
+        for i in range(0,n_nos_tot):
+            if CC[i] == 1:
+                for j in range(0,n_nos_tot):
+                    if j!=i:
+                        Kg[i,j] = 0
+                        if CC[j]==0:
+                            Fg[j] = Fg[j] - Kg[j,i]*Fg[i]
+                            Kg[j,i] = 0
+
+        return Kg,Fg
+
+class ans:
+    def deslocamento(n_nos_tot,n_el,n_nos_el,x,integralAdx,E,conec,Fc,f,xCC,valor_CC):
+        Kg,Fg = Global.Kg_Fg_contorno(n_nos_tot,n_el,n_nos_el,x,integralAdx,E,conec,Fc,f,xCC,valor_CC)
+        Kg_inv = np.linalg.inv(Kg)
+        Fg = (Fg.T)
+        u = np.matmul(Kg_inv,Fg)
+        return u
+
+
+
+#class FatorForma: para vigas
 #    def N1(xe,xe1,xe2):return 1 - 3*((xe-xe1)/(xe2-xe1))**2 + 2*((xe-xe1)/(xe2-xe1))**3
 #    def N2(xe,xe1,xe2):return -3*(xe-xe1)*(1-(xe-xe1)/(xe2-xe1))**2
 #    def N3(xe,xe1,xe2):return 3*((xe-xe1)/(xe2-xe1))**2 - 2*((xe-xe1)/(xe2-xe1))**3
--- Python_Codes/Python_Tests/TesteFEM.py
+++ Python_Codes/Python_Tests/TesteFEM.py
@@ -1,22 +1,32 @@
 import numpy as np
-from ..FEM import Elem
+from ..FEM import Elem, Global, ans
 import unittest
 
 class Test_FEM(unittest.TestCase):
     def testFEM_1GL(self): # 1grau de liberdade por nó - cálculo do deslocamento apenas
     # ENTRADA DE DADOS
-        n_el = 2; #numero de elementos da estrutura (AB e BC)
-        n_nos_el = np.array([3,2]); #número de nós por elemento
+        n_el = 2 #numero de elementos da estrutura (AB e BC)
+        n_nos_el = np.array([3,2]) #número de nós por elemento
         x = np.array([1., 3., 5.]); xA = x[0]; xB = x[1]; xC = x[2]
-        n_nos_tot = sum(n_nos_el) - (n_el-1); #número total de nós da estrutura
+        n_nos_tot = sum(n_nos_el) - (n_el-1) #número total de nós da estrutura
 
-        E = 2e7*np.ones(n_el);
+        E = 2e7*np.ones(n_el)
+
+        Fc = np.array([0, 0, 150., 0])
+        f = np.array([10.,0])
 
         def integralAdx(he,i):
             if i==0: return 0.1*he
             else: return 0.3
 
         conec = np.array([[1,2,3], [3,4,0]])    ##barra AB tem 3nós e a BC tem 2. - caso tenha alguma outra coisa doida assim, preferi deixar logo pra o usuario definir
-    # INICIALIZAÇÃO
-        Kg = Elem.Kg(n_el,n_nos_tot,n_nos_el,x,integralAdx,E,conec)
-        print(Kg)
+
+        # posição e valor das condições de contorno
+        xCC = np.array([0,n_nos_tot-1]) #posição
+        valor_CC = np.array([0, 0])
+
+        u = ans.deslocamento(n_nos_tot,n_el,n_nos_el,x,integralAdx,E,conec,Fc,f,xCC,valor_CC)
+
+        u_ans = np.array([0.,3.45E-5,6.4E-5,0.])
+        for i in range(len(u)):
+            self.assertAlmostEqual(u[i],u_ans[i],3,'ValueError: Failed')
