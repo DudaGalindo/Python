070f537ee4fcb125a4359885d7a712013356e9bd
DudaGalindo <meduardagalindo8@gmail.com>
2019-10-21 23:22:59 -0300

    Untill vigas, but I think the code can be improoved

--- Python_Codes/Python_Tests/TesteFEM.py
+++ Python_Codes/Python_Tests/TesteFEM.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import unittest
 
 class Test_FEM(unittest.TestCase):
+
     def testFEM_Barra_prob5_4(self): # 1grau de liberdade por nó - cálculo do deslocamento apenas
     # ENTRADA DE DADOS
         n_el = 2 #numero de elementos da estrutura (AB e BC)
@@ -54,8 +55,9 @@ class Test_FEM(unittest.TestCase):
         # Condições de Contorno:
         xCC_dir = np.array([0,1,16,17]) #posição
         valor_CCdir = np.array([0, 0, 0, 0])
+        x = np.linspace(0,L,n_el+1);
 
-        u = Viga.deslocamento(n_el,L,I,E,Fc,xFc,q,xCC_dir,valor_CCdir,cf) #1GL
+        u = Viga.deslocamento(n_el,x,I,E,Fc,xFc,q,xCC_dir,valor_CCdir,cf) #1GL
 
         u_ans = np.array([0.,0.,1.5025E-5,-8.4188E-5,3.0318E-5,3.3573E-5,2.7515E-5,6.7402E-5,1.7364E-20,1.3429E-4,-2.7615E-5,6.7402E-5,-3.0318E-5,-3.3573E-5,-1.5025E-5,-8.4188E-5,0.,0.])
 
@@ -63,13 +65,14 @@ class Test_FEM(unittest.TestCase):
             self.assertAlmostEqual(u[i],u_ans[i],3,'ValueError: Failed')
         ##Plotagens:
         #Viga.Plots(x,u,L,n_el,E,I)
+
     def test_trelica7_4_1(self):
         n_el = 2;
         E = 3E7*np.ones(n_el)
         A = np.array([0.4,0.5])
 
         n_nos_el = 2*np.ones(n_el)
-        n_nos_tot = int(sum(n_nos_el) - (n_el-1))
+        n_nos_tot = general.n_nosTOTAL(n_nos_el,n_el)
 
         # Condições de Contorno:
         xCC = np.array([0,1,4,5]) #dos nós
@@ -79,9 +82,45 @@ class Test_FEM(unittest.TestCase):
         Fc = np.array([-1E3])
         xFc = np.array([3])
 
-        coord  = np.array([[0.,0.],[10.,0.],[0.,10.]]) #[x,y]
-        u = Trelica.deslocamento(coord,n_nos_tot,Fc,xFc,xCC,valor_CC,n_el,A,E)
-        print(u)
+        coord_no  = np.array([[0.,0.],[10.,0.],[0.,10.]]) #[x,y]
+        conec_el = np.array([[1,2],[2,3]])
+        u = Trelica.deslocamento(coord_no,conec_el,n_nos_tot,n_nos_el,Fc,xFc,xCC,valor_CC,n_el,A,E)
+
         u_ans = np.array([0.0000,0.0000,-0.0008,-0.0027,0.0000,0.0000])
+
         for i in range(len(u)):
             self.assertAlmostEqual(u[i],u_ans[i],4,'ValueError: Failed')
+
+    def test_trelica7_4_2(self): ##rever
+        n_el = 9
+        E = 200e9*np.ones(n_el)
+        A = 0.0025*np.ones(n_el)
+
+        n_nos_el = 2*np.ones(n_el)
+        n_nos_tot = 6
+
+        # Forças concentradas:
+        Fc = np.array([-600,200])
+        xFc = np.array([7,8])
+
+        # Condições de Contorno:
+        xCC = np.array([0,1,11])
+        valor_CC = np.array([0,0,0])
+
+        coord_no = np.array([[0,0],[4,0],[4,3],[8,0],[8,3],[12,0]])
+        conec_el = np.array([[1,2],[1,3],[2,3],[2,4],[3,4],[3,5],[4,5],[4,6],[5,6]])
+
+        u = Trelica.deslocamento(coord_no,conec_el,n_nos_tot,n_nos_el,Fc,xFc,xCC,valor_CC,n_el,A,E)
+
+        u_ans = np.array([0.0000,0.0000E-5,0.3200E-5,-1.57E-5,0.8650E-5,-1.5700E-5,0.6400E-5,-2.2867E-5,0.5450E-5,-2.0167E-5,1.12000E-5,0.0000])
+
+        for i in range(len(u)-1):
+            self.assertAlmostEqual(u[i],u_ans[i],9,'ValueError: Failed')
+
+    def testeFRAME(self):
+        E = 210000
+        A = 100
+        n_el = 3
+        n_no_el = 2*np.array(n_el)
+        coord_no = np.array([[0,3],[3,3],[6,0],[9,0]])
+        
