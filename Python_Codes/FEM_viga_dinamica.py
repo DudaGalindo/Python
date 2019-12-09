281b212539c7173c89883f7da45bd8cb21b1bac0
DudaGalindo <meduardagalindo8@gmail.com>
2019-12-09 20:31:23 -0300

    Working untill D inversion

--- Python_Codes/FEM_viga_dinamica.py
+++ Python_Codes/FEM_viga_dinamica.py
@@ -1,25 +1,25 @@
 ''' Código para análise dinâmica de vigas'''
 
 import numpy as np
-import scipy as scp
-from FEM import general
+from scipy.linalg import eig
+from .FEM_viga import Viga
+from .FEM import general
 import time
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 class VigaDin:
-    def initialize():
+    def initialize(ngl_tot):
         Mg = np.zeros([ngl_tot,ngl_tot])
         Cg = np.zeros([ngl_tot,ngl_tot])
         Kg,Fg = general.initialize(ngl_tot)
         return Kg,Fg,Mg,Cg
 
-    def Kg_Fg_Mg(xCC,valor_CC,Kg,Fg,Mg):
+    def Kg_Mg_Cg(xCC,valor_CC,Kg,Mg,Cg,ngl_tot):
         CC = np.zeros(ngl_tot)
 
         for i in range(0,len(xCC)):
             CC[xCC[i]] = 1
-            Fg[xCC[i]] = valor_CC[i]
             Kg[xCC[i],xCC[i]] = 1
             Mg[xCC[i],xCC[i]] = 1
             Cg[xCC[i],xCC[i]] = 1
@@ -32,13 +32,12 @@ class VigaDin:
                         Mg[i,j] = 0
                         Cg[i,j] = 0
                         if CC[j]==0:
-                            Fg[j] = Fg[j] - Kg[j,i]*Fg[i]
                             Kg[j,i] = 0
                             Mg[j,i] = 0
                             Cg[j,i] = 0
-        return Kg,Fg,Mg
+        return Kg,Mg,Cg
 
-    def ElemMass(ro,A,he,I):
+    def ElemMass(rho,A,he,I):
         m1 = np.array([[156,-22*he,54,13*he],\
                       [-22*he,4*he**2,-13*he,-3*he**2],\
                       [54,-13*he,156,22*he],\
@@ -46,15 +45,16 @@ class VigaDin:
         m2 = np.array([[36,-3*he,-36,-3*he],\
                       [-3*he,4*he**2,3*he,-he**2],\
                       [-36,3*he,36,3*he],\
-                      [3*he,-he**2,3*he,4*he**2]])
-        Mel = ro*A*he/420*m1 + ro*I/(30*he)*m2
+                      [-3*he,-he**2,3*he,4*he**2]])
+
+        Mel = rho*A*he/420*m1 + rho*I/(30*he)*m2
         return Mel
 
-    def Global(E,ro,I,cf,Fc,x,Kg,Fg,Mg,Cg,n_el,ngl_el,conec,q,alpha,beta):
+    def Global(E,rho,A,I,cf,Fc,x,Kg,Fg,Mg,Cg,n_el,ngl_el,conec,q,alpha,beta):
         for el in range(0,n_el):
             he = abs(x[el] - x[el+1])
             Kel, Fel = Viga.Elem(E,I,he,cf,q,x[el],x[el+1])
-            Mel = VigaDin.ElemMass(ro,A,he,I)
+            Mel = VigaDin.ElemMass(rho,A,he,I)
             Cel = alpha*Kel + beta*Mel #NÃO ENTENDI PORQUÊ
 
             for i in range(0,ngl_el):
@@ -69,7 +69,7 @@ class VigaDin:
         Fg = Fg + Fc
         return Kg,Fg,Mg,Cg
 
-    def deslocamento(E,I,cf,x,xCC,valor_CC,xF,F,n_el,q,alpha,beta):
+    def deslocamento(E,rho,A,I,cf,x,xCC,valor_CC,xF,F,n_el,q,alpha,beta):
         n_nos_el,n_nos_tot,ngl_no = Viga.init(n_el)
         ngl_el, ngl_tot = general.ngl(ngl_no,n_nos_el[0],n_nos_tot)
         conec = general.conect(ngl_tot,n_el,ngl_el,ngl_no)
@@ -78,36 +78,55 @@ class VigaDin:
         Fc[xF[:]] = F[:]
 
         Kg,Fg,Mg,Cg = VigaDin.initialize(ngl_tot)
-        Kg,Fg,Mg,Cg = Viga.Global(E,I,cf,Fc,x,Kg,Fg,Mg,n_el,ngl_el,conec,q,alpha,beta)
-        Kg,Fg,Mg,Cg = VigaDin.Kg_Fg_Mg(xCC,valor_CC,Kg,Fg,Mg,Cg)
+        Kg,Fg,Mg,Cg = VigaDin.Global(E,rho,A,I,cf,Fc,x,Kg,Fg,Mg,Cg,n_el,ngl_el,conec,q,alpha,beta)
+        Kg,Mg,Cg = VigaDin.Kg_Mg_Cg(xCC,valor_CC,Kg,Mg,Cg,ngl_tot)
 
         ''' Encontrando os autovalores e autovetores '''
-        lamb,phi = scp.linalg.eig(Kg*inv(Mg))
-        w = lamb**(1/2)
+        lamb,psi = eig(Kg,Mg)
+        w = lamb**(1/2); w = w.real
+        psi = psi.real
 
         '''Tornando a matriz modal psi ortonormal à de massa'''
-        PSI = np.zeros(psi.shape)
-        for r in range(psi[:,0]):
-            PSI[:,r] = psi[:,r]/sqrt(Mg[r,r])
+        MgD = (psi.T)@Mg@psi
 
+        PSI = np.zeros(psi.shape)
+        for r in range(0,ngl_tot-1):
+            PSI[:,r] = psi[:,r]/(MgD[r,r]**(1/2))
+
+        ''' pré e pós multiplicando os termos'''
+        Mg = (PSI.T)@Mg@PSI
+        Cg = (PSI.T)@Cg@PSI
+        Kg = (PSI.T)@Kg@PSI
+        for i in range(0,ngl_tot):
+            for j in range(0,ngl_tot):
+                if j!=i:
+                    Mg[i,j] = 0
+                    Kg[i,j] = 0
+                    Cg[i,j] = 0
+
+        ## Zerando as colunas que são ínfimas mas podem influenciar
+
+        #print(Kg)
+        #print(w**2)
         ''' Setting for plot '''
-        X = np.zeros(len(Fg))
+        X = np.zeros(ngl_tot)
+        x = np.linspace(0,1,ngl_tot)
         fig,ax = plt.subplots(1)
         line, = plt.plot(x, X, 'r-')
-        ax.set_xlim(0, x[n_el-1])
+        ax.set_xlim(0, 1)
         ax.set_ylim(-1, 1)
 
-        for i in range(len(w)):
-            D = -w[i]**2*PSI.T*Mg*PSI + 1j*w[i]*PSI.T*C*PSI + PSI.T*Kg*PSI
-            H = PSI*inv(D)*PSI.T
-            X = H*Fg
+        ''' Computing X'''
+        for i in range(0,len(w)):
+            D = -((w[i])**2)*Mg + 1j*w[i]*Cg + Kg #solve problem: D is returning as a singular matrix
+            H = PSI*np.linalg.inv(D)*PSI.T
+            for a in range(0,len(H[:,0])):
+                for b in range(0,len(H[0,:])):
+                    H[a,b] = np.linalg.norm(H[a,b])
 
+            H = H.real
+            X = H@Fg
             line.set_ydata(X)
             fig.canvas.draw_idle()
             fig.canvas.start_event_loop(.05)
             plt.show(block=False)
-
-        #lamb = sp.symbols('lambda')
-        #cp = sp.det(Kg-lamb*Mg)
-        #eigs = sp.solveset(cp,lamb)
-        #eigs_v2 = sp.solveset(cp.rewrite(sp.exp).simplify(), lam)
--- Python_Codes/Python_Tests/TesteFEMdin.py
+++ Python_Codes/Python_Tests/TesteFEMdin.py
@@ -2,17 +2,24 @@
 
 import numpy as np
 from ..FEM_viga_dinamica import VigaDin
+from ..FEM_viga import Viga
+from ..FEM import general
 import unittest
 
 class TestVigasDinamica(unittest.TestCase):
-    def test6(self):
-        alfa = 0.000000001
-        beta = 0.000000005
+    def test3_general_case(self):
+        ''' Dados de entrada '''
         n_el = 50 #numero de elementos da estrutura
         L = 13*0.305 #m
         E = 200e9 #constante
         I = 50e-6 #constante
+        rho = 7800
+        A = 0.01
 
+        alpha = 0.000000001
+        beta = 0.000000005
+
+        ''' Carga distribuída '''
         def q(x):
             q0 = 250*4.448/0.305
             q1 = 150*4.448/0.305
@@ -20,23 +27,23 @@ class TestVigasDinamica(unittest.TestCase):
                 return q0*x/(8*0.305)
             else: return q0+q1*(x-8*0.305)/(5*0.305)
 
-        # Forças Concentradas:
+        ''' Forças Concentradas '''
         n_el_trechos = (np.array([8/13,2/13,3/13])*n_el).round().astype(int)
         xFc = np.array([2*n_el_trechos[0],2*(n_el_trechos[0]+n_el_trechos[1])])
         Fc = np.array([3000,-15000])*4.448
 
         cf = 0;
 
-        # Condições de Contorno:
+        ''' Condições de Contorno '''
         n_nos_el,n_nos_tot,ngl_no = Viga.init(n_el)
         ngl_el, ngl_tot = general.ngl(ngl_no,n_nos_el[0],n_nos_tot)
         xCC_dir = np.array([ngl_tot-2,ngl_tot-1]) #posição
         valor_CCdir = np.array([0, 0])
 
-        # Vetor x:
+        ''' Vetor x '''
         x1 = np.linspace(0,8*0.305,n_el_trechos[0]+1)
         x2 = np.linspace((8+2/n_el_trechos[1])*0.305,10*0.305,n_el_trechos[1])
         x3 = np.linspace((10+3/n_el_trechos[2])*0.305,L,n_el_trechos[2])
         x = np.append(x1,x2); x = np.append(x,x3)
 
-        VigaDin.deslocamento(E,I,cf,x,xCC_dir,valor_CCdir,xFc,Fc,n_el,q,alpha,beta)
+        VigaDin.deslocamento(E,rho,A,I,cf,x,xCC_dir,valor_CCdir,xFc,Fc,n_el,q,alpha,beta)
